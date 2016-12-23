d785bd2b8c1fecc33a204a587a31f8e0e5021cec
Robert Molnar <mail.robertmolnar@gmail.com>
2016-12-23 20:55:30 +0000

    passport.js, db.js added

---
+++ passport.js
@@ -0,0 +1,31 @@
+const knex = require('./db')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+
+passport.use(new LocalStrategy(authenticate))
+
+function authenticate(email, password, done) {
+  db("users")
+    .where("email", email)
+    .first()
+    .then((user) => {
+      if(!user || user.password !== password) {
+        return done(null, false, {message: "wrong user or password"})
+      }
+
+      done(null, user)
+    }, done)
+}
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id)
+})
+
+passport.deserializeUser(function(id, done) {
+  db("users")
+    .where("id", id)
+    .first()
+    .then((user) => {
+      done(null, user)
+    }, done )
+})
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const session = require('express-session')
+const passport = require('passport')
+const db = require('./db')
+require('./passport')
 
 express()
   .use(session({ secret: "You like cats", resave: false, saveUninitialized: false }))
+  .use(passport.initialize())
+  .use(passport.session())
   .get("/", (req, res, next) => {
     res.send(req.session)
   })
