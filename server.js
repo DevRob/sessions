f632b2a6554a1b960240303de3cd66ec17479f6e
Robert Molnar <mail.robertmolnar@gmail.com>
2016-12-26 17:23:24 +0000

    server.js upd

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const session = require('express-session')
 const passport = require('passport')
 const bodyParser = require('body-parser')
+const authRoutes = require('./routes/auth.js')
+const postsRoutes = require('./routes/posts.js')
 const db = require('./db')
 require('./passport')
 
@@ -12,6 +14,8 @@ express()
   .use(session({ secret: "You like cats", resave: false, saveUninitialized: false }))
   .use(passport.initialize())
   .use(passport.session())
+  .use(authRoutes)
+  .use(postsRoutes)
   .get("/", (req, res, next) => {
     res.send({
       session: req.session,
@@ -19,23 +23,4 @@ express()
       authenticated: req.isAuthenticated(),
     })
   })
-  .get("/login", (req, res, next) => {
-    res.render("login")
-  })
-  .post("/login", passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-  }))
-  .get("/logout", (req, res, next) => {
-    req.session.destroy((err) => {
-      res.redirect("login")
-    })
-  })
-  .get("/signup", (req, res, next) => {
-    res.render("signup")
-  })
-  .post("/signup", passport.authenticate("local-register", {
-    successRedirect: "/",
-    failureRedirect: "/signup",
-  }))
   .listen(3033)
